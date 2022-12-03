4dc81e04d18b1421737d659b4e1e3e9482960947
Matthew O'Neill <matthew.oneill@airelogic.com>
2022-12-03 13:03:35 +0000

    add python implementation

---
+++ day3/day3.py
@@ -0,0 +1,22 @@
+import string
+
+scores = {letter: score for score, letter in enumerate(string.ascii_letters,1)}
+
+def split_strings(row):
+    lenstr = len(row)//2
+    return row[:lenstr], row[lenstr:]
+
+def intersections(*tups):
+    tups = iter(tups) 
+    first = set(next(tups))
+    for tup in tups:
+        first &= set(tup)
+    return first.pop()
+
+with open("puzzle.input") as f:
+    print("part1 :", sum(scores[intersections(*tup)] for tup in map(split_strings, f.readlines())))
+
+
+with open("puzzle.input") as f:
+    lines = [line.strip() for line in f.readlines()]
+    print("part2 :",sum(scores[intersections(*tup)] for tup in zip(lines[::3], lines[1::3], lines[2::3]) ))
