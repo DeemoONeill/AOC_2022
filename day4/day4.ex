9f757a523795fde51e03899788bc0f65b96542c6
Matthew O'Neill <matthew.oneill@airelogic.com>
2022-12-04 10:19:16 +0000

    day4.ex

---
+++ day4/day4.ex
@@ -0,0 +1,39 @@
+defmodule Assignments do
+  def split_into_assignments(row) do
+    row
+    |> String.split(",")
+    |> Enum.map(&Assignments.parse_assignment/1)
+  end
+
+  def part(inputs, func) do
+    for row <- inputs do
+      split_into_assignments(row) |> func.()
+    end
+    |> Enum.count(& &1)
+  end
+
+  def parse_assignment(string) do
+    string
+    |> String.trim()
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def contains?([[start1, end1], [start2, end2]]) do
+    (start1 >= start2 and end1 <= end2) or (start1 <= start2 and end1 >= end2)
+  end
+
+  def overlaps?([[start1, end1], [start2, end2]] = row) do
+    contains?(row) or (end1 >= start2 and end1 <= end2) or (end2 >= start1 and end2 <= end1)
+  end
+end
+
+"puzzle.input"
+|> File.stream!()
+|> Assignments.part(&Assignments.contains?/1)
+|> IO.inspect()
+
+"puzzle.input"
+|> File.stream!()
+|> Assignments.part(&Assignments.overlaps?/1)
+|> IO.inspect()
