366eb4c00615b5d03c802a4fd117e9c63e830e37
Matthew O'Neill <oneill_91@hotmail.co.uk>
2022-12-06 06:54:03 +0000

    add day6

---
+++ day6/day6.ex
@@ -0,0 +1,41 @@
+defmodule Signal do
+  def find_signal([_head | tail] = input, num_chars) do
+    if unique_chars(input, num_chars) == num_chars do
+      1
+    else
+      tail
+      |> find_signal(num_chars, 2)
+    end
+  end
+
+  defp find_signal([], _num_chars, count), do: count
+
+  defp find_signal([_head | tail] = input, num_chars, count) do
+    if unique_chars(input, num_chars) do
+      find_signal([], num_chars, count + num_chars - 1)
+    else
+      find_signal(tail, num_chars, count + 1)
+    end
+  end
+
+  def unique_chars(input, num_chars) do
+    MapSet.new(
+      input
+      |> Enum.take(num_chars)
+    )
+    |> MapSet.to_list()
+    |> length() == num_chars
+  end
+end
+
+"puzzle.input"
+|> File.read!()
+|> String.graphemes()
+|> Signal.find_signal(4)
+|> IO.puts()
+
+"puzzle.input"
+|> File.read!()
+|> String.graphemes()
+|> Signal.find_signal(14)
+|> IO.puts()
