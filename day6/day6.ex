990bf92f577482f18a4039f75872c76ccde9dac4
Matthew O'Neill <oneill_91@hotmail.co.uk>
2022-12-06 07:06:27 +0000

    make elixir code more idiomatic

--- day6/day6.ex
+++ day6/day6.ex
@@ -1,6 +1,6 @@
 defmodule Signal do
   def find_signal([_head | tail] = input, num_chars) do
-    if unique_chars(input, num_chars) == num_chars do
+    if unique_chars?(input, num_chars) == num_chars do
       1
     else
       tail
@@ -11,14 +11,14 @@ defmodule Signal do
   defp find_signal([], _num_chars, count), do: count
 
   defp find_signal([_head | tail] = input, num_chars, count) do
-    if unique_chars(input, num_chars) do
+    if unique_chars?(input, num_chars) do
       find_signal([], num_chars, count + num_chars - 1)
     else
       find_signal(tail, num_chars, count + 1)
     end
   end
 
-  def unique_chars(input, num_chars) do
+  def unique_chars?(input, num_chars) do
     MapSet.new(
       input
       |> Enum.take(num_chars)
