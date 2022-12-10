f00812120752e93cd23944f76d233f8cf540e381
Matthew O'Neill <oneill_91@hotmail.co.uk>
2022-12-10 09:00:35 +0000

    refactor and tidy up day 10

--- day10/day10.ex
+++ day10/day10.ex
@@ -6,35 +6,34 @@ defmodule SignalStrength do
     end
   end
 
-  def run_instruction({0, num}, register, cycle, cycles) do
+  def run_instruction(instruction), do: run_instruction(instruction, 1, 1, [])
+
+  def run_instruction({0, num}, cycle, register, cycles) do
     [{cycle, register + num} | cycles]
   end
 
-  def run_instruction({cycles_remaining, num}, register, cycle, cycles) do
+  def run_instruction({cycles_remaining, num}, cycle, register, cycles) do
     {cycles_remaining - 1, num}
-    |> run_instruction(register, cycle + 1, [{cycle, register} | cycles])
+    |> run_instruction(cycle + 1, register, [{cycle, register} | cycles])
   end
 
-  def calculate_registers(instructions) do
-    for instruction <- instructions, reduce: [] do
-      [] ->
-        SignalStrength.run_instruction(instruction, 1, 1, [])
-
+  def calculate_registers([first | instructions]) do
+    for instruction <- instructions, reduce: run_instruction(first) do
       [{cycle, register} | tail] ->
-        SignalStrength.run_instruction(instruction, register, cycle, tail)
+        run_instruction(instruction, cycle, register, tail)
     end
     |> Enum.reverse()
   end
 
-  def calculate_screen_position(positions) do
-    for {{_, register}, cycle} <- positions |> Enum.with_index(), reduce: [] do
+  def calculate_screen_position(positions, hit_char \\ "#", miss_char \\ ".") do
+    for {cycle, register} <- positions, reduce: [] do
       acc ->
         sprite_position = (register - 1)..(register + 1)
 
-        if rem(cycle, 40) in sprite_position do
-          ["#" | acc]
+        if rem(cycle - 1, 40) in sprite_position do
+          [hit_char | acc]
         else
-          ["." | acc]
+          [miss_char | acc]
         end
     end
     |> Enum.reverse()
@@ -42,7 +41,8 @@ defmodule SignalStrength do
 
   def part1(registers) do
     registers
-    |> Enum.filter(&((&1 |> elem(0)) in [20, 60, 100, 140, 180, 220]))
+    |> Enum.drop(19)
+    |> Enum.take_every(40)
     |> Enum.map(fn {cycle, register} -> cycle * register end)
     |> Enum.sum()
     |> IO.inspect(label: "part1")
@@ -60,10 +60,9 @@ defmodule SignalStrength do
 end
 
 "puzzle.input"
-|> File.read!()
-|> String.split("\n")
+|> File.stream!()
 |> Enum.map(&SignalStrength.parse_instruction/1)
 |> SignalStrength.calculate_registers()
 |> SignalStrength.part1()
-|> SignalStrength.calculate_screen_position()
+|> SignalStrength.calculate_screen_position("##", "  ")
 |> SignalStrength.part2()
